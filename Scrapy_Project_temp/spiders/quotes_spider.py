5452b61bf5ffb2fd3ed286b7d4aa93c5384c853b
NGimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2019-07-03 08:10:30 +0530

    Update with TechNews site

--- Scrapy_Project_temp/spiders/quotes_spider.py
+++ Scrapy_Project_temp/spiders/quotes_spider.py
@@ -5,25 +5,36 @@ class QuotesSpider(scrapy.Spider):
 
     name = "quotes"
     start_urls = [
-        "http://www.lankadeepa.lk/politics/13",
-        "http://www.lankadeepa.lk/world_news/14",
-        "http://www.lankadeepa.lk/business/9",
-        "http://www.lankadeepa.lk/feature/2",
-        "http://www.lankadeepa.lk/top_story/10"
+        "https://technews.lk/tag/technology/",
+        "https://technews.lk/tag/google/",
+        "https://technews.lk/tag/mobile/",
+        "https://technews.lk/tag/apps-tag/",
+        "https://technews.lk/tag/security/",
+        "https://technews.lk/tag/business/",
+        "https://technews.lk/tag/social-media/"
+
+
+
 
     ]
 
     def parse(self, response):
-        allNews = response.css('header.post-content div.row div.simple-thumb')
-        newsCategory = response.css('div.col-xs-18 div.header')
-        for news in allNews:
+        allBlogs = response.css('main.site-main div.inner div.post-feed article.post-card')
+        for blog in allBlogs:
             yield {
-                'newsCategory':newsCategory.css('h4.catpost-category::text').get().encode("utf-8"),
-                'newsTitle': news.css('a::attr(href)').get().encode("utf-8"),
-                'newsTime': news.css('p.timeandauthor small::text').get().encode("utf-8"),
-                'summary': news.css('p.catexcerpt::text').get().encode("utf-8")
+                'blogCategory':blog.css('div.post-card-content '
+                                        'a.post-card-content-link '
+                                        'header.post-card-header '
+                                        'span.post-card-tags::text').get().encode("utf-8"),
+                'blogTitle': blog.css('div.post-card-content '
+                                      'a.post-card-content-link '
+                                      'header.post-card-header '
+                                      'h2.post-card-title::text').get().encode("utf-8"),
+                'author': blog.css('div.post-card-content footer.post-card-meta ul.author-list li.author-list-item div.author-name-tooltip::text').get().encode("utf-8"),
+                'summary': blog.css('div.post-card-content a.post-card-content-link header.post-card-header p.post-card-excerpt::text').get().encode("utf-8"),
+                'blogDate': blog.css('div.post-card-content footer.post-card-meta span.reading-time::text').get().encode('utf-8')
             }
 
-        next_page = response.css('div.page-nation ul.pagination li.next a::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        # next_page = response.css('div.page-nation ul.pagination li.next a::attr(href)').get()
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self.parse)
